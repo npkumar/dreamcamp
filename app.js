0fada0e61285485527ed5001ffb2eb1da3999faf
Nitin Pradeep Kumar <getnpk@gmail.com>
2016-12-14 09:37:18 +0000

    add custom stylesheet and link public folder

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ seedDB();
 mongoose.connect("mongodb://localhost/dream_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 
 app.get("/", function(req, res){
