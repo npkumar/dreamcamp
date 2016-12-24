6b738f55b7d3fbb79ffd0225433c22e74bf451fa
Nitin Pradeep Kumar <getnpk@gmail.com>
2016-12-24 05:18:06 +0000

    add method-override

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express     = require("express"),
     mongoose    = require("mongoose"),
     passport    = require("passport"),
     LocalStrategy = require("passport-local"),
+    methodOverride = require("method-override"),
     User        = require("./models/user"),
     seedDB      = require("./seeds");
 
@@ -18,6 +19,7 @@ mongoose.connect("mongodb://localhost/dream_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
 
 // PASSPORT
 app.use(require("express-session")({
